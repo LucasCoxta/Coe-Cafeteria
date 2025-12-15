31b2c1ad1a75c66d9d529afd78d9e00b1f03a963
LucasCoxta <lucas.cdomingos@gmail.com>
2025-12-14 22:50:44 -0300

    preparaçoes finais

--- assets/js/pedido.js
+++ assets/js/pedido.js
@@ -1,37 +1,25 @@
 export function initpedido() {
-    document.addEventListener("DOMContentLoaded", () => {
-        function openWhatsApp(number, message) {
-            const url = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
-            window.open(url, "_blank");
-        }
-        const mainCards = document.querySelectorAll(".cards-container .card");
+  document.addEventListener("click", (e) => {
+    const card = e.target.closest(".card");
 
-        mainCards.forEach(card => {
-            card.addEventListener("click", () => {
+    if (!card) {
+      document
+        .querySelectorAll(".card")
+        .forEach((c) => c.classList.remove("active"));
+      return;
+    }
 
-                const titulo = card.querySelector("h3")?.textContent || "Pedido";
+    document
+      .querySelectorAll(".card")
+      .forEach((c) => c.classList.remove("active"));
 
-                openWhatsApp(
-                    "5561981904208",
-                    `Olá! Gostaria de ver mais opções da categoria: ${titulo}.`
-                );
+    card.classList.add("active");
+  });
 
-            });
-        });
-        const cardWhats = document.querySelector(".pedido-card.whatsapp");
-        if (cardWhats) {
-            cardWhats.addEventListener("click", () => {
-                openWhatsApp("5561912341234", "Olá! Quero fazer um pedido.");
-            });
-        }
-        const cardIfood = document.querySelector(".pedido-card.ifood");
-        if (cardIfood) {
-            cardIfood.addEventListener("click", () => {
-                window.open("https://www.ifood.com.br", "_blank");
-            });
-        }
-
-    });
+  document.addEventListener("click", (e) => {
+    if (e.target.closest(".cardOP-btn")) {
+      e.stopPropagation();
+      console.log("Botão clicado");
+    }
+  });
 }
-
-
